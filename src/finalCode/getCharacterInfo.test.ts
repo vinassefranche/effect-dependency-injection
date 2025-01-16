63d631665bc66f34788a29185cf503a25296ae84
vinassefranche <vincent.francois@inato.com>
2025-01-16 11:23:47 +0100

    Remove effect vitest

--- src/finalCode/getCharacterInfo.test.ts
+++ src/finalCode/getCharacterInfo.test.ts
@@ -1,12 +1,11 @@
 import assert from "node:assert";
-import { describe, vi } from "vitest";
+import { describe, vi, it } from "vitest";
 
 import { CharacterInfoService, getCharacterInfo } from "./getCharacterInfo.ts";
 import { Effect } from "effect";
-import { it } from "@effect/vitest";
 
 describe("getCharacterInfo", () => {
-  it.effect("should return the correct character info", () => {
+  it("should return the correct character info", () => {
     return Effect.gen(function* () {
       const characterInfoService = yield* CharacterInfoService;
       vi.spyOn(characterInfoService, "getCharacterById").mockImplementationOnce(
@@ -24,6 +23,6 @@ describe("getCharacterInfo", () => {
       const characterInfo = yield* getCharacterInfo(1);
       assert.equal(characterInfo.name, "Luke Skywalker");
       assert(!characterInfo.isDarthVader);
-    }).pipe(Effect.provide(CharacterInfoService.dummyLayer));
+    }).pipe(Effect.provide(CharacterInfoService.dummyLayer), Effect.runPromise);
   });
 });
--- src/getCharacterInfo.test.ts
+++ src/getCharacterInfo.test.ts
@@ -1,16 +1,15 @@
 import assert from "node:assert";
-import { describe } from "vitest";
+import { describe, it } from "vitest";
 import { getCharacterInfo } from "./getCharacterInfo.ts";
 import { Effect } from "effect";
 import { FetchHttpClient } from "@effect/platform";
-import { it } from "@effect/vitest";
 
 describe("getCharacterInfo", () => {
-  it.effect("should return the correct character info", () => {
+  it("should return the correct character info", () => {
     return Effect.gen(function* () {
       const characterInfo = yield* getCharacterInfo(1);
       assert.equal(characterInfo.name, "Luke Skywalker");
       assert(!characterInfo.isDarthVader);
-    }).pipe(Effect.provide(FetchHttpClient.layer));
+    }).pipe(Effect.provide(FetchHttpClient.layer), Effect.runPromise);
   });
 });
